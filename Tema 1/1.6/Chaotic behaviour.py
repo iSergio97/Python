7f0200fd3c01024a6360c9833fb66cc139251c88
iSergio97 <sergiotb15@gmail.com>
2019-01-30 15:40:08 +0100

    Initial commit

---
+++ Tema 1/1.6/Chaotic behaviour.py
@@ -0,0 +1,16 @@
+
+
+def main():
+    print("Testing the chaotic behaviour")
+    x = eval(input("Enter a number between 0 and 1: "))
+    if x < 0 or x > 1:
+        pass
+        print("The number is less than 0 or higher than 1")
+    else:
+        for i in range(10):
+            x = 3.9 * x * (1 - x)
+            print(x)
+
+
+#Note: Leave to blank lines between the end function and the next instruction
+main()
---
+++ Tema 1/1.6/hello_world.py
@@ -0,0 +1,12 @@
+#print("Hello World!")
+#print(2 + 3)
+#print("2 + 3 =", 2 + 3)
+#print("Añade espacios", "tras las comillas")
+
+
+# Primera función
+def myFirstFunction():
+    print("My first function")
+
+
+
